82bbb9f9f0a6908d047b5cb8f2d124e9e27e6ca1
Takuto Y <takuto.yoshikai@gmail.com>
2017-11-06 10:12:42 +0900

    11/6変更分

--- p2p.js
+++ p2p.js
@@ -2,9 +2,33 @@ let express = require("express");
 let app = express();
 let bodyParser = require("body-parser");
 let request = require("request");
+let myPort = parseInt(process.argv[2]);
+let multer = require("multer");
+let multiparty = require("multiparty");
+let fs = require("fs");
+let restler = require("restler");
+let distributedFileName = [
+
+];
+let storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, "./files/" + myPort);
+	},
+	filename: function(req, file, cb) {
+		cb(null, file.originalname);
+	},
+});
+let upload = multer({ storage: storage,
+	fileFilter: function(req, file, cb) {
+		if (distributedFileName.indexOf(file.originalname) == -1) {
+			cb(null, true);
+		}
+		return cb(null, false);
+	}
+}).single("file");
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
 function selectRandomValues(array, c) {
 	let count = c;
 	if (count > array.length) {
@@ -32,6 +56,7 @@ function half(array) {
 function onlyUnique(value, index, self) { 
 	return self.indexOf(value) == index;
 }
+
 
 class P2P {
 	init(port) {
@@ -62,7 +87,6 @@ class P2P {
 		});
 
 		app.post("/peer", function(req, res) {
-			console.log("post peer");
 			let peer = req.body.peer;
 			if (self.peers.indexOf(peer) == -1) {
 				if (self.peers.length > 20) {
@@ -90,6 +114,21 @@ class P2P {
 			res.status(200).send(self.data);
 		});
 
+		app.post("/file", function(req, res) {
+			//TODO ここのレスポンスは後で直す
+			upload(req, res, function(err) {
+				if (err || req.file == null) {
+					res.status(400).send();
+					return;
+				}
+				distributedFileName.push(req.file.originalname);
+				setTimeout(function(){
+					self.broadcastFile(req.file.originalname, self.peers);
+				}, 1000);
+				console.log(self.port + "が画像を保存しました");
+				res.status(200).send();
+			});
+		});
 	}
 
 	sendData(data) {
@@ -101,25 +140,6 @@ class P2P {
 				if (!error && response.statusCode == 200) {
 					console.log(self.port + "が " + peer + "に" + data + "を送りました");
 				}
-			});
-		});
-	}
-	registerSelf(url) {
-		let self = this;
-		return new Promise(function(resolve, reject) {
-			request.post({
-				url: url + "/peer",
-				json: true,
-				headers: {  'Content-Type': 'application/json' },
-				body: {
-					peer: self.url
-				}
-			}, function(error, response, body) {
-				if (!error && response.statusCode == 201) {
-					resolve();
-					return;
-				}
-				reject();
 			});
 		});
 	}
@@ -191,17 +211,33 @@ class P2P {
 			});
 		});
 	}
+	broadcastFile(fileName) {
+		let self = this;
+		let path = "./files/" + myPort + "/" + fileName;
+		fs.stat(path, function(err, stats) {
+			let file = restler.file(path, null, stats.size, null, "text/plain");
+			self.peers.forEach(function(peer) {
+				restler.post(peer + "/file", {
+					multipart: true,
+					data: {
+						"file": file
+					}
+				}).on("complete", function(data) {
+				});
+			});
+		});
+	}
 
 	start() {
 		let self = this;
 		app.listen(self.port, function() {
-			console.log("serverが起動しました");
+			console.log("Peerが起動しました");
 			console.log("ポート: " + self.port);
 		})
 	}
 }
 
-let p2p = new P2P(parseInt(process.argv[2]));
+let p2p = new P2P(myPort);
 p2p.start();
 
 if (p2p.port != 8000) {
